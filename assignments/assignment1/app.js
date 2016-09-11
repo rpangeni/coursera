bba6a735db6ceea33ba4da63f6cb3ade7b0783e6
rpangeni <r_pangeni@yahoo.com>
2016-09-11 11:53:03 +1000

    Small change to update the logic

--- assignments/assignment1/app.js
+++ assignments/assignment1/app.js
@@ -13,10 +13,10 @@ angular.module('LunchCheck', [])
     $scope.checkIfTooMuch = function (){
       var realItems = $scope.items.split(",").filter(function(a) { return a.trim();});
       //remove any empty items
-      
+
       if (realItems.length > 3){
         $scope.message = "Too much";
-      } else if ($scope.items.length == 0){
+      } else if (realItems.length == 0){
         $scope.message = "Please enter data first.";
       } else if (realItems.length <= 3){
         $scope.message = "Enjoy!.";
