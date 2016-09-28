4671547c2f18e80defb1aed045cf7ce049386823
Ramakanta Pangeni <rama.pangeni@objective.com>
2016-09-28 13:27:15 +1000

    Changes regarding the error message

--- assignments/assignment3/src/narrow.controller.js
+++ assignments/assignment3/src/narrow.controller.js
@@ -5,7 +5,7 @@ angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .directive("foundItems", FoundItemsDirective)
 .service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 function FoundItemsDirective(){
     var ddo = {
